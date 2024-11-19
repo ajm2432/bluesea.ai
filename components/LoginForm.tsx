861138eca0251e596721f05d0a950299910c5045
Austin Miller <amiller.craig@gmail.com>
2024-11-18 23:26:30 -0500

    add file upload UI component

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -62,15 +62,13 @@ export default function LoginForm({
                         </div>
                         {error && <div style={styles.error}>{error}</div>}
                         <div style={styles.inputGroup}> 
-                       Username:
-                            <Field id="username" name="username" placeholder="Enter your Username" style={styles.input} />
+                            <Field id="username" name="username" placeholder="Username" style={styles.input} />
                             <div style={styles.error}>
                                 <ErrorMessage name="username" component="span" />
                             </div>
                         </div>
                         <div style={styles.inputGroup}>
-                        Password:
-                            <Field type="password" id="password" name="password" placeholder="Enter your Password" style={styles.input} />
+                            <Field type="password" id="password" name="password" placeholder="Password" style={styles.input} />
                             <div style={styles.error}>
                                 <ErrorMessage name="password" component="span" />
                             </div>
@@ -138,7 +136,7 @@ const styles = {
         margin: 0,
     },
     inputGroup: {
-        marginBottom: '1rem',
+        marginBottom: 'rem',
     },
     input: {
         width: '100%',
